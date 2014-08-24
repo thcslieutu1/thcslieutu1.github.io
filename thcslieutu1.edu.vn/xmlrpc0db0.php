a5f95a8d7f7276d5690b4bc3dd6f31ffdfbd46f8
doimoigiaoduc <doimoigiaoduc@gmail.com>
2014-08-25 06:09:30 +0700

    Sao luu dữ liệu

---
+++ thcslieutu1.edu.vn/xmlrpc0db0.php
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+  <service>
+    <engineName>WordPress</engineName>
+    <engineLink>http://wordpress.org/</engineLink>
+    <homePageLink>http://thcslieutu1.edu.vn</homePageLink>
+    <apis>
+      <api name="WordPress" blogID="1" preferred="true" apiLink="http://thcslieutu1.edu.vn/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="http://thcslieutu1.edu.vn/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://thcslieutu1.edu.vn/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="http://thcslieutu1.edu.vn/xmlrpc.php" />
+          </apis>
+  </service>
+</rsd>
